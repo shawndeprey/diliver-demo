c407848776b81b659694602ae39375e4ca1f2860
Shawn Deprey <shawndeprey@gmail.com>
2013-05-31 00:27:04 -0700

    Walked through page setup. Setup first 2 transaction pages.

--- app/controllers/demo/default_controller.rb
+++ app/controllers/demo/default_controller.rb
@@ -1,8 +1,16 @@
 class Demo::DefaultController < ApplicationController
 
+	# GET /
   def index
   	respond_to do |format|
       format.html { } # index.html.erb
+    end
+  end
+
+  # GET /team-identification
+  def team_identification
+  	respond_to do |format|
+      format.html { } # team_identification.html.erb
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Diliver::Application.routes.draw do
   namespace :demo do
     get '/' => 'default#index'
+    get '/team-identification' => 'default#team_identification'
   end
 end
