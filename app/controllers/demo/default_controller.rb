03634a986e474f53ee526f81876b22e3ba965270
Justin Hammond <justin.35f@gmail.com>
2013-05-31 04:43:59 -0700

    Set up routes for all initial pages. Added functions to default controller. Created all 7 initial pages.

--- app/controllers/demo/default_controller.rb
+++ app/controllers/demo/default_controller.rb
@@ -14,4 +14,46 @@ class Demo::DefaultController < ApplicationController
     end
   end
 
+  # GET /buyer-qualification
+  def buyer_qualification
+    respond_to do |format|
+      format.html { } # buyer_qualification.html.erb
+    end
+  end
+
+  # GET /seller-qualification
+  def seller_qualification
+    respond_to do |format|
+      format.html { } # seller_qualification.html.erb
+    end
+  end
+
+  # GET /prelim-dd
+  def prelim_dd
+    respond_to do |format|
+      format.html { } # prelim_dd.html.erb
+    end
+  end
+
+  # GET /initial-valuation
+  def initial_valuation
+    respond_to do |format|
+      format.html { } # initial_valuation.html.erb
+    end
+  end
+
+  # GET /confirm-dd
+  def confirm_dd
+    respond_to do |format|
+      format.html { } # confirm_dd.html.erb
+    end
+  end
+
+  # GET /final-valuation
+  def final_valuation
+    respond_to do |format|
+      format.html { } # final_valuation.html.erb
+    end
+  end
+
 end
