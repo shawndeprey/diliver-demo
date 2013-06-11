32c06e5e767618c59db6bd47787ca56fa9c7d105
Shawn Deprey <shawndeprey@gmail.com>
2013-06-10 22:29:48 -0700

    Added framework for confirmatory due diligence.

--- app/assets/javascripts/diliver.js
+++ app/assets/javascripts/diliver.js
@@ -5,6 +5,24 @@
       		// Initializing our JS Library Settings
       	}
       },
+      confirmDD: {
+        changeToTab: function(open_class){
+          /*
+          diliver.confirmDD.closeAllTables();
+          diliver.toggleElement(open_class);
+          */
+        },
+        closeAllTables: function(){
+          /*
+          diliver.toggleElement("tab_1_class");
+          diliver.toggleElement("tab_2_class");
+          diliver.toggleElement("tab_3_class");
+          diliver.toggleElement("tab_4_class");
+          diliver.toggleElement("tab_5_class");
+          ...
+          */
+        }
+      },
       init: function(){
       	diliver.settings.init();
       },
