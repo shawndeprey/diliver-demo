adb760ae0e931c91bfc1e2b2ac6e27759cd17fa6
Justin Hammond <justin.35f@gmail.com>
2013-06-13 19:19:16 -0700

    Second phase of work.

--- app/assets/javascripts/diliver.js
+++ app/assets/javascripts/diliver.js
@@ -10,6 +10,27 @@
       },
       toggleElement: function(element_class){
       	$("."+element_class).slideToggle(250);
+      },
+      toggleScorecard: function(id_to_show){
+        $(".scorecard").each(function(index){
+          if($(this).attr("id") == id_to_show){
+            $(this).delay(500).show(250);
+          }
+          else{
+            $(this).hide(250);
+          }
+        });
+      },
+      toggleTransaction: function(select){
+        var value = select.options[select.selectedIndex].value;
+        $(".transaction").each(function(index){
+          if($(this).attr("id") == value){
+            $(this).delay(500).show(250);
+          }
+          else{
+            $(this).hide(250);
+          }
+        });
       }
     };
     global.diliver = diliver;
