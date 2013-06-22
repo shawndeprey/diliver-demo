b3cecf5cbdc412998dd45cfa18368ce27b38b266
Shawn Deprey <shawndeprey@gmail.com>
2013-06-22 10:27:36 -0700

    I made what should be the final tweaks for this demo.

--- app/assets/javascripts/diliver.js
+++ app/assets/javascripts/diliver.js
@@ -5,24 +5,6 @@
       		// Initializing our JS Library Settings
       	}
       },
-      confirmDD: {
-        changeToTab: function(open_class){
-          /*
-          diliver.confirmDD.closeAllTables();
-          diliver.toggleElement(open_class);
-          */
-        },
-        closeAllTables: function(){
-          /*
-          diliver.toggleElement("tab_1_class");
-          diliver.toggleElement("tab_2_class");
-          diliver.toggleElement("tab_3_class");
-          diliver.toggleElement("tab_4_class");
-          diliver.toggleElement("tab_5_class");
-          ...
-          */
-        }
-      },
       init: function(){
       	diliver.settings.init();
       },
@@ -30,14 +12,28 @@
       	$("."+element_class).slideToggle(250);
       },
       toggleScorecard: function(id_to_show){
-        $(".scorecard").each(function(index){
+        // This function is intended to control the navigation of our scorecard flow.
+        // First we grab our objects, ensuring we supply ample information for fast dom processing
+        scorecards = $("div.scorecard");
+        scorecards.each(function(){
           if($(this).attr("id") == id_to_show){
-            $(this).delay(500).show(250);
-          }
-          else{
-            $(this).hide(250);
+            scorecard_to_show = $(this)
           }
         });
+        upload_button = $('a#round_2_document');
+
+        // Next we hide all scorecards and show the one scorecard we intend to if the we are changing cards
+        if(!$(scorecard_to_show).is(":visible")){
+          scorecards.hide(250);
+          scorecard_to_show.delay(500).show(250);
+        }
+
+        //Finally we clean up our button. We don't want to show it one 1 of the 15 pages
+        if(id_to_show == 'ewbs'){
+          upload_button.hide();
+        } else {
+          upload_button.show();
+        }
       },
       toggleTransaction: function(select){
         var value = select.options[select.selectedIndex].value;
