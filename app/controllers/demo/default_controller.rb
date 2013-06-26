076b94a625ea94c22862d14a516a55a7a2b3be66
Shawn Deprey <shawndeprey@gmail.com>
2013-06-25 22:15:51 -0700

    Hopefully this is the final pull for the tweaks :)

--- app/controllers/demo/default_controller.rb
+++ app/controllers/demo/default_controller.rb
@@ -19,7 +19,7 @@ class Demo::DefaultController < ApplicationController
                              {title:"Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence", is_active:false, is_current:false},
                              {title:"Valuation Preparation", url:"/demo/valuation-preparation", is_active:false, is_current:false},
                              {title:"Final Valuation", url:"/demo/final-valuation", is_active:false, is_current:false}],
-                   next_button:[{title:"Proceed to Next Step: Transaction Analysis", url:"/demo/transaction-analysis"}]}
+                   next_button:[{title:"Next Step: Transaction Analysis", url:"/demo/transaction-analysis"}]}
   	respond_to do |format|
       format.html { } # team_identification.html.erb
     end
@@ -37,7 +37,7 @@ class Demo::DefaultController < ApplicationController
                              {title:"Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence", is_active:false, is_current:false},
                              {title:"Valuation Preparation", url:"/demo/valuation-preparation", is_active:false, is_current:false},
                              {title:"Final Valuation", url:"/demo/final-valuation", is_active:false, is_current:false}],
-                   next_button:[{title:"Proceed to Next Step: Screening", url:"/demo/screening"}]}
+                   next_button:[{title:"Next Step: Screening", url:"/demo/screening"}]}
     respond_to do |format|
       format.html { } # transaction_analysis.html.erb
     end
@@ -55,7 +55,7 @@ class Demo::DefaultController < ApplicationController
                              {title:"Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence", is_active:false, is_current:false},
                              {title:"Valuation Preparation", url:"/demo/valuation-preparation", is_active:false, is_current:false},
                              {title:"Final Valuation", url:"/demo/final-valuation", is_active:false, is_current:false}],
-                   next_button:[{title:"Proceed to Next Step: Qualification", url:"/demo/qualification"}]}
+                   next_button:[{title:"Next Step: Qualification", url:"/demo/qualification"}]}
     respond_to do |format|
       format.html { } # screening.html.erb
     end
@@ -73,7 +73,7 @@ class Demo::DefaultController < ApplicationController
                              {title:"Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence", is_active:false, is_current:false},
                              {title:"Valuation Preparation", url:"/demo/valuation-preparation", is_active:false, is_current:false},
                              {title:"Final Valuation", url:"/demo/final-valuation", is_active:false, is_current:false}],
-                   next_button:[{title:"Proceed to Next Step: Preliminary Due Diligence", url:"/demo/preliminary-due-diligence"}]}
+                   next_button:[{title:"Next Step: Preliminary Due Diligence", url:"/demo/preliminary-due-diligence"}]}
     respond_to do |format|
       format.html { } # qualification.html.erb
     end
@@ -91,7 +91,7 @@ class Demo::DefaultController < ApplicationController
                              {title:"Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence", is_active:false, is_current:false},
                              {title:"Valuation Preparation", url:"/demo/valuation-preparation", is_active:false, is_current:false},
                              {title:"Final Valuation", url:"/demo/final-valuation", is_active:false, is_current:false}],
-                   next_button:[{title:"Proceed to Next Step: Initial Valuation", url:"/demo/initial-valuation"}]}
+                   next_button:[{title:"Next Step: Initial Valuation", url:"/demo/initial-valuation"}]}
     respond_to do |format|
       format.html { } # preliminary_due_diligence.html.erb
     end
@@ -109,7 +109,7 @@ class Demo::DefaultController < ApplicationController
                              {title:"Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence", is_active:false, is_current:false},
                              {title:"Valuation Preparation", url:"/demo/valuation-preparation", is_active:false, is_current:false},
                              {title:"Final Valuation", url:"/demo/final-valuation", is_active:false, is_current:false}],
-                   next_button:[{title:"Proceed to Next Step: Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence"}]}
+                   next_button:[{title:"Next Step: Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence"}]}
     respond_to do |format|
       format.html { } # initial_valuation.html.erb
     end
@@ -126,7 +126,7 @@ class Demo::DefaultController < ApplicationController
                              {title:"Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence", is_active:true, is_current:true},
                              {title:"Valuation Preparation", url:"/demo/valuation-preparation", is_active:false, is_current:false},
                              {title:"Final Valuation", url:"/demo/final-valuation", is_active:false, is_current:false}],
-                   next_button:[{title:"Proceed to Next Step: Valuation Preparation", url:"/demo/valuation-preparation"}]}
+                   next_button:[{title:"Next Step: Valuation Preparation", url:"/demo/valuation-preparation"}]}
     respond_to do |format|
       format.html { } # confirmatory_due_diligence.html.erb
     end
@@ -143,7 +143,7 @@ class Demo::DefaultController < ApplicationController
                              {title:"Confirmatory Due Diligence", url:"/demo/confirmatory-due-diligence", is_active:true, is_current:false},
                              {title:"Valuation Preparation", url:"/demo/valuation-preparation", is_active:true, is_current:true},
                              {title:"Final Valuation", url:"/demo/final-valuation", is_active:false, is_current:false}],
-                   next_button:[{title:"Proceed to Next Step: Final Valuation", url:"/demo/final-valuation"}]}
+                   next_button:[{title:"Next Step: Final Valuation", url:"/demo/final-valuation"}]}
     respond_to do |format|
       format.html { } # valuation_preparation.html.erb
     end
